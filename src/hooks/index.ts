2172043f7d1bf5647125c52201f7b0d951088f4c
geo318 <geo.lomidze@gmail.com>
2023-03-31 18:24:53 +0400

    feat: remove body scrolling when popup is open

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,3 @@
 export * from './useMagnifier';
 export * from './useScreenWidth';
+export * from './useToggleBodyScroll';
--- src/pages/Gallery/useGallery.ts
+++ src/pages/Gallery/useGallery.ts
@@ -1,8 +1,11 @@
+import { useToggleBodyScroll } from 'hooks';
 import { useState } from 'react';
 
 export const useGallery = () => {
   const [hovered, setHovered] = useState('');
   const [imageIndex, setImageIndex] = useState<number | null>();
+  useToggleBodyScroll({ toggle: !!imageIndex });
+
   const close = () => setImageIndex(null);
 
   return { hovered, setHovered, imageIndex, setImageIndex, close };
