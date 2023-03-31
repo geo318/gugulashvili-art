192267aa2c747ba6378106c463d1723349383177
geo318 <geo.lomidze@gmail.com>
2023-03-31 14:46:56 +0400

    style: homepage mobile design

--- src/Components/Navigate/Navigate.tsx
+++ src/Components/Navigate/Navigate.tsx
@@ -4,15 +4,16 @@ import { Button } from 'Components/Button';
 
 export const Navigate = ({ link }: { link: string }) => {
   return (
-    <div className='flex mt-auto justify-end mb-10'>
+    <div className='flex mt-auto lg:justify-end justify-center text-center mb-10'>
       <Link
         to={link}
-        className='mr-16 hover:opacity-80 hover:scale-105 active:scale-95 transition-all duration-300'
+        className='lg:mr-16 mt-auto hover:opacity-80 hover:scale-105 active:scale-95 transition-all duration-300'
       >
         <motion.div
           initial={{ scale: 0.9, opacity: 0 }}
           animate={{ scale: 1, opacity: 0.5 }}
           transition={{ duration: 0.5, delay: 1.5 }}
+          t
         >
           <Button />
         </motion.div>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -11,17 +11,17 @@ export const Home = () => (
       exit={{ opacity: 0, y: '-100vh' }}
       transition={{ duration: 0.8 }}
     >
-      <div className='pt-28 text-txt-green flex flex-col h-full'>
+      <div className='lg:pt-28 pt-16 text-txt-green flex flex-col h-full'>
         <motion.div
           className='flex items-center justify-center flex-col'
           initial={{ opacity: 0, scale: 0.9 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5, delay: 1 }}
         >
-          <h1 className='text-[3.25rem] leading-[3.25rem] font-medium'>
+          <h1 className='text-[2rem] leading-[2rem] lg:text-[3.25rem] lg:leading-[3.25rem] font-medium'>
             Zura Gugulashvili
           </h1>
-          <div className='flex text-sm gap-8 mt-5 font-normal'>
+          <div className='flex mb-auto lg:flex-row flex-col-reverse items-center text-sm gap-8 mt-5 font-normal'>
             <span>Artist</span>
             <span>January 24, 1973</span>
             <span className='underline'>zuragugulashvili73@gmail.com</span>
