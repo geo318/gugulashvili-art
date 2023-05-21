981e447aeb66a2d54ff5ded54659007bb59b7a56
geo318 <geo.lomidze@gmail.com>
2023-05-21 16:27:19 +0400

    add external links: artfourm and biennale

--- src/assets/images/externals/index.ts
+++ src/assets/images/externals/index.ts
@@ -1,7 +1,21 @@
 import ZuraGugulashvili from './ZuraGugulashvili.jpg';
 import Paint from './Paint.jpg';
+import artforum from './artforum.png';
+import biennale from './biennale.png';
 
 export const externals = [
+  {
+    heading: null,
+    logo: null,
+    avatar: artforum,
+    link: 'https://www.artforum.com/news/52nd-international-venice-biennale-artist-list-announced-12907',
+  },
+  {
+    heading: null,
+    logo: null,
+    avatar: biennale,
+    link: 'https://universes.art/fileadmin/user_upload/Biennials/Venice/2007/Pavilions-Venice-Biennale-2007.pdf',
+  },
   // {
   //   heading: artMine,
   //   logo: agora,
