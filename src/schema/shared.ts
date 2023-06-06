d973600a67921095509193166853477360aa70ae
geo318 <geo.lomidze@gmail.com>
2023-06-06 16:45:34 +0400

    increase max size of image upload to 5mb

--- src/schema/shared.ts
+++ src/schema/shared.ts
@@ -1,10 +1,10 @@
 import z from 'zod';
 
-const MAX_SIZE = 2000000;
+const MAX_SIZE = 5 * 1024 * 1024;
 const MIME_TYPES = ['image/jpg', 'image/jpeg', 'image/png', 'image/webp'];
 
 export const imgSchema = z
   .instanceof(FileList)
   .refine((file: FileList) => file?.length, 'please, select an image')
-  .refine((file) => file[0]?.size <= MAX_SIZE, 'image is too big')
+  .refine((file) => file[0]?.size <= MAX_SIZE, 'image must be less than 5mb')
   .refine((file) => MIME_TYPES.includes(file[0]?.type), 'incorrect file type');
